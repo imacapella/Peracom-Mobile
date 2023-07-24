e085ec31cdcf97d45a7d7921f4ac50febd1ad3a5
imacapella <gurkankaradas42@gmail.com>
2023-07-24 10:35:51 +0300

    First Commit to Project, Home and Login Page

---
+++ Peracom Trial/HomePage.swift
@@ -0,0 +1,58 @@
+//
+//  HomePage.swift
+//  Peracom Trial
+//
+//  Created by Gürkan Karadaş on 21.07.2023.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View{
+        VStack{
+            Image("logo")
+                .padding(.top, 65)
+            Spacer()
+            ZStack{
+                VStack{
+                    HStack(spacing: 40){
+                        HomeWidgets(widgetText: "Report", iconName: "newspaper") {
+                            print("sleam")
+                        }
+                        .padding()
+                        HomeWidgets(widgetText: "Print", iconName: "printer") {
+                            print("sleam")
+                        }
+                    }
+                    HStack(spacing: 40){
+                        HomeWidgets(widgetText: "Graphics", iconName: "chart.xyaxis.line") {
+                            print("sleam")
+                        }
+                        .padding()
+                        HomeWidgets(widgetText: "Tasks", iconName: "list.clipboard") {
+                            print("sleam")
+                        }
+                    }
+                    HStack(spacing: 40){
+                        HomeWidgets(widgetText: "Profile", iconName: "person.crop.circle") {
+                            print("sleam")
+                        }
+                        .padding()
+                        HomeWidgets(widgetText: "help", iconName: "questionmark.circle") {
+                            print("sleam")
+                        }
+                    }
+                } .padding(.trailing, 20)
+                
+                
+            }
+            Spacer()
+        }
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
---
+++ Peracom Trial/Peracom_TrialApp.swift
@@ -0,0 +1,18 @@
+//
+//  Peracom_TrialApp.swift
+//  Peracom Trial
+//
+//  Created by Gürkan Karadaş on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Peracom_TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginPage()
+            HomePage()
+        }
+    }
+}
